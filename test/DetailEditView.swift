ddc6e94aeb1af60194cc9f5b40c77432810785f5
axy-moon <augxy666@gmail.com>
2024-01-08 18:07:42 +0530

    error in detailView

--- test/DetailEditView.swift
+++ test/DetailEditView.swift
@@ -8,7 +8,8 @@
 import SwiftUI
 
 struct DetailEditView: View {
-    @State private var scrum = DailyScrum.emptyScrum
+    
+    @Binding var scrum = DailyScrum
     @State private var newAttendeeName = ""
     var body: some View {
         Form {
@@ -49,6 +50,8 @@ struct DetailEditView: View {
     }
 }
 
-#Preview {
-    DetailEditView()
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView(scrum: .constant(DailyScrum.sampleData[0]))
+    }
 }
